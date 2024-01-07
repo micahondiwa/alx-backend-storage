dc0b91022c39bb4756d6bef80e91a441ab11d333
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-07 11:01:17 +0300

    Fix

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
-"""8-all.py module"""
-
-from pymongo import MongoClient
-
-list_all = __import__("8-all").list_all
+"""Task 8's module.
+"""
 
 
-if __name__ == "__main__":
-    client = MongoClient("mongodb://127.0.0.1:27017")
-    school_collection = client.my_db.school
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {}".format(school.get("_id"), school.get("name")))
+def list_all(mongo_collection):
+    """Lists all documents in a collection."""
+    return [doc for doc in mongo_collection.find()]
