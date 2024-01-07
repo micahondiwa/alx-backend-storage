7bece125ab12febcf6fe991637ab24b03fe54c8b
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-07 12:25:41 +0300

    Execercise

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
-""" Main file """
+"""
+Main file
+"""
+import redis
 
 Cache = __import__("exercise").Cache
 
 cache = Cache()
 
-s1 = cache.store("first")
-print(s1)
-s2 = cache.store("secont")
-print(s2)
-s3 = cache.store("third")
-print(s3)
+# data = b"hello"
+# key = cache.store(data)
+# print(key)
 
-inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
-outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
+# local_redis = redis.Redis()
+# print(local_redis.get(key))
 
-print("inputs: {}".format(inputs))
-print("outputs: {}".format(outputs))
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
+
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
