cfc5143e201c23c84c6f11716b9f681b762c90f6
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-07 05:27:57 +0300

    Best band ever

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- SQL script that ranks country origin of bands
 
-SELECT origin, SUM(fnas) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
     FROM metal_bands
     GROUP BY origin
     ORDER BY nb_fans DESC;
